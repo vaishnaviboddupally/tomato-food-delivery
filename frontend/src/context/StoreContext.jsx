bd1d38ea1957865ed0c11bc29b3d9bfd2afca287
vaishnaviboddupally <85918991+vaishnaviboddupally@users.noreply.github.com>
2024-06-25 18:50:58 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://tomato-food-delivery-1bm7.onrender.com/";
+  const url = "https://tomato-food-delivery-1bm7.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
